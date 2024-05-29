dd5eb1b18a37eb5b121664f843ab2c5da440f9a7
LIMoreno04 <ignamoreno04@gmail.com>
2024-05-29 20:03:47 -0300

    Usar Hash cerrado en vez de encadenado

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import Entities.Cancion;
 import Entities.LectorCSV;
 import uy.edu.um.adt.Exceptions.InvalidValue;
 import uy.edu.um.adt.hash.MyChainedHashImpl;
+import uy.edu.um.adt.hash.MyClosedHashImpl;
 
 import java.io.FileNotFoundException;
 
@@ -10,7 +11,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 
 
-        MyChainedHashImpl<String,Cancion> Datos;
+        MyClosedHashImpl<String,Cancion> Datos;
         try {
             String filePath = "C:/Users/Nacho/IdeaProjects/universal_top_spotify_songs.csv";
             Datos = LectorCSV.hashDeDatos(filePath);
